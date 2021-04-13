ec28de4b77462718c3d3a57f5582bea13bfdd9f0
Alan Morgan <alanxoc3@gmail.com>
2021-04-12 23:03:05 -0600

    Created most of the basic commands.

--- README.md
+++ README.md
@@ -8,43 +8,33 @@ Api idea:
 ttrack 
 ttrack --help
 ttrack help
-
-ttrack --version
 ttrack version
 
 ttrack rec <group-name> --timeout=1d1h1m1s
-ttrack add <group-name> 2021-01-03 10m30s
+ttrack set <group-name> 2021-01-03 10m30s
 
-ttrack mv <group-name>... <group-name>
+ttrack mv <group>... <group>
+ttrack del <group> --begin-date=2021-01-01 --end-date=2021-01-03
 
-ttrack del <group-name> --begin=2021-01-01 --end=2021-01-03
-ttrack sub <group-name> --date=2021-01-03 10m30s
-
-ttrack list
+ttrack groups
   <group-name-1>
   <group-name-2>
   ...
 
-ttrack format <group-name-1> <group-name-2>... --format='%g: %d %h %m %s' --begin=2021-01-01 --end=2021-01-10
+ttrack list <group> --begin-date=2021-01-01 --end-date=2021-01-04
+  2021-01-01: 10m
+  2021-01-02: 13s
+  2021-01-03: 30m
+  2021-01-04: 1h
+
+ttrack agg <group> --begin-date=2021-01-01 --end-date=2021-01-04
+  1h40m13s
+  0s
 ```
 
 Config file:
 ```
---timeout=10m
-```
-
-Format meanings:
-```
-%g = group name
-%d = number of days.
-%h = number of hours.
-%m = number of minutes.
-%s = number of seconds.
-
-%D = total time in days.
-%H = total time in hours.
-%M = total time in minutes.
-%S = total time in seconds.
+default_timeout=30s
 ```
 
 Database format idea:
--- main.go
+++ main.go
@@ -2,14 +2,47 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var version string = "snapshot"
+var DEFAULT_TIMEOUT time.Duration = time.Duration(30) * time.Second
+
+type date time.Time
+
+func newDate(val time.Time, p *time.Time) *date {
+	*p = val
+	return (*date)(p)
+}
+
+func (d *date) Set(s string) error {
+	v, err := time.Parse("2006-01-02", s)
+	*d = date(v)
+	return err
+}
+
+func (d *date) Type() string { return "date" }
+
+func (d *date) String() string {
+    if (*time.Time) (d).IsZero() {
+        return ""
+    } else {
+        return (*time.Time)(d).Format("2021-12-25")
+    }
+}
 
 func main() {
+	app := &cobra.Command{
+		Use:   "ttrack [command]",
+		Short: "A time tracking program.",
+	}
 
+	beginDate := newDate(time.Time{}, &time.Time{})
+	endDate := newDate(time.Time{}, &time.Time{})
+
+	//------ VERSION COMMAND
 	versionCmd := &cobra.Command{
 		Use:   "version",
 		Short: "print build information",
@@ -17,11 +50,100 @@ func main() {
 			fmt.Println("ttrack " + version)
 		},
 	}
-
-	app := &cobra.Command{
-		Use:   "ttrack [subcommand] [flags] args",
-		Short: "A time tracking program.",
-	}
 	app.AddCommand(versionCmd)
+
+	//------ REC COMMAND
+	var timeout time.Duration
+	recCmd := &cobra.Command{
+		Use:   "rec",
+		Short: "record current time",
+		Run: func(cmd *cobra.Command, args []string) {
+    			fmt.Println("Recording")
+    			fmt.Println(timeout.String())
+		},
+		Args: cobra.ExactArgs(1),
+	}
+
+	recCmd.Flags().DurationVarP(&timeout, "timeout", "t", DEFAULT_TIMEOUT, "timeout when to end recording")
+	app.AddCommand(recCmd)
+
+	//------ MV COMMAND
+	mvCmd := &cobra.Command{
+		Use:   "mv [flags] <srcGroup>... <dstGroup>",
+		Short: "move/merge/rename groups",
+		Run: func(cmd *cobra.Command, args []string) {
+    			fmt.Println("moving")
+		},
+		Args: cobra.MinimumNArgs(2),
+	}
+	app.AddCommand(mvCmd)
+
+	//------ DEL COMMAND
+	delCmd := &cobra.Command{
+		Use:   "del [flags] <group>",
+		Short: "delete a group or date range in a group",
+		Run: func(cmd *cobra.Command, args []string) {
+    			fmt.Println("bd: " + beginDate.String())
+    			fmt.Println("ed: " + endDate.String())
+		},
+		Args: cobra.ExactArgs(1),
+	}
+
+	delCmd.Flags().VarPF(beginDate, "begin-date", "b", "begin date for deletion range")
+	delCmd.Flags().VarPF(endDate, "end-date", "e", "end date for deletion range")
+	app.AddCommand(delCmd)
+
+	//------ GROUPS COMMAND
+	groupsCmd := &cobra.Command{
+		Use:   "groups",
+		Short: "show available groups",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("listing groups all right")
+		},
+		Args: cobra.ExactArgs(0),
+	}
+	app.AddCommand(groupsCmd)
+
+	//------ LIST COMMAND
+	listCmd := &cobra.Command{
+		Use:   "list",
+		Short: "list dates with associated duration",
+		Run: func(cmd *cobra.Command, args []string) {
+    			fmt.Println("bd: " + beginDate.String())
+    			fmt.Println("ed: " + endDate.String())
+		},
+		Args: cobra.ExactArgs(1),
+	}
+
+	listCmd.Flags().VarPF(beginDate, "begin-date", "b", "only list dates after this")
+	listCmd.Flags().VarPF(endDate, "end-date", "e", "only list dates before this")
+	app.AddCommand(listCmd)
+
+	//------ AGG COMMAND
+	aggCmd := &cobra.Command{
+		Use:   "agg",
+		Short: "aggregate dates for range into single duration",
+		Run: func(cmd *cobra.Command, args []string) {
+    			fmt.Println("bd: " + beginDate.String())
+    			fmt.Println("ed: " + endDate.String())
+		},
+		Args: cobra.ExactArgs(1),
+	}
+
+	aggCmd.Flags().VarPF(beginDate, "begin-date", "b", "only aggregate dates after this")
+	aggCmd.Flags().VarPF(endDate, "end-date", "e", "only aggregate dates before this")
+	app.AddCommand(aggCmd)
+
+	//------ SET COMMAND
+	setCmd := &cobra.Command{
+		Use:   "set <group> <date> <duration>",
+		Short: "sets the duration for a group's date",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("set set set.")
+		},
+		Args: cobra.ExactArgs(3),
+	}
+	app.AddCommand(setCmd)
+
 	app.Execute()
 }
