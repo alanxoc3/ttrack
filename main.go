f98f7468b98540050a83a7e3efe92e4438e31365
Alan Morgan <alanxoc3@gmail.com>
2021-05-16 19:25:45 -0600

    Reorganizing command options.

--- main.go
+++ main.go
@@ -11,57 +11,45 @@ import (
 
 var version string = "snapshot"
 
+type params struct {
+	beginDate dateArg
+	endDate dateArg
+	recursive bool
+	daily bool
+	quote bool
+	groups []string
+	date dateArg
+	duration seconds.Seconds
+}
+
 func main() {
 	app := &cobra.Command{
-		Use:   "ttrack [command]",
-		Short: "A time tracking program.",
 	}
 
-	beginDate := dateArg{}
-	endDate := dateArg{}
-	recursive := false
-	daily := false
-	escape := false
+    c := params{}
 
-	//------ VERSION COMMAND
 	versionCmd := &cobra.Command{
-		Use:   "version",
-		Short: "print build information",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("ttrack " + version)
 		},
 	}
-	app.AddCommand(versionCmd)
 
-	//------ TIDY COMMAND
 	tidyCmd := &cobra.Command{
-		Use:   "tidy",
-		Short: "clean up all the files ttrack uses",
+		Args: cobra.ExactArgs(0),
         Run: func(cmd *cobra.Command, args []string) {
     		// TODO: Implement me.
 		},
-		Args: cobra.ExactArgs(0),
 	}
 
-	app.AddCommand(tidyCmd)
-
-	//------ LS COMMAND
 	listCmd := &cobra.Command{
-		Use:   "ls [<group>]...",
-		Short: "list groups",
+		Args: cobra.ExactArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
 			listFunc()
 		},
-		Args: cobra.ExactArgs(0),
 	}
-	listCmd.Flags().BoolVarP(&recursive, "recursive", "r", false, "list all subgroups too")
-	listCmd.Flags().BoolVarP(&escape, "quote", "q", false, "quote each group according to posix shell quoting rules")
-	app.AddCommand(listCmd)
 
-	//------ REC COMMAND
 	recCmd := &cobra.Command{
-		Use:   "rec <group> <duration>",
-		Short: "record current time",
+		Args: cobra.ExactArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			dur, durerr := time.ParseDuration(args[1])
 			if durerr != nil {
@@ -70,15 +58,10 @@ func main() {
 
 			recFunc(args[0], seconds.CreateFromDuration(dur))
 		},
-		Args: cobra.ExactArgs(2),
 	}
 
-	app.AddCommand(recCmd)
-
-	//------ SET COMMAND
 	setCmd := &cobra.Command{
-		Use:   "set <group> <date> <duration>",
-		Short: "sets the duration for a group's date",
+		Args: cobra.ExactArgs(3),
 		Run: func(cmd *cobra.Command, args []string) {
 			g := args[0]
 			ts, tserr := date.CreateFromString(args[1])
@@ -93,14 +76,10 @@ func main() {
 
 			setFunc(g, *ts, seconds.Seconds(dur.Milliseconds()/1000))
 		},
-		Args: cobra.ExactArgs(3),
 	}
-	app.AddCommand(setCmd)
 
-	//------ ADD COMMAND
 	addCmd := &cobra.Command{
-		Use:   "add <group> <date> <duration>",
-		Short: "adds the duration for a group's date",
+		Args: cobra.ExactArgs(3),
 		Run: func(cmd *cobra.Command, args []string) {
 			g := args[0]
 			ts, tserr := date.CreateFromString(args[1])
@@ -115,14 +94,10 @@ func main() {
 
 			setFunc(g, *ts, seconds.Seconds(dur.Milliseconds()/1000))
 		},
-		Args: cobra.ExactArgs(3),
 	}
-	app.AddCommand(addCmd)
 
-	//------ SUB COMMAND
 	subCmd := &cobra.Command{
-		Use:   "sub <group> <date> <duration>",
-		Short: "subtracts the duration for a group's date",
+		Args: cobra.ExactArgs(3),
 		Run: func(cmd *cobra.Command, args []string) {
 			g := args[0]
 			ts, tserr := date.CreateFromString(args[1])
@@ -137,68 +112,88 @@ func main() {
 
 			setFunc(g, *ts, seconds.Seconds(dur.Milliseconds()/1000))
 		},
-		Args: cobra.ExactArgs(3),
 	}
-	app.AddCommand(subCmd)
 
-	//------ MV COMMAND
 	mvCmd := &cobra.Command{
-		Use:   "mv <source-group>... <destination-group>",
-		Short: "rename/merge groups",
-		Run: func(cmd *cobra.Command, args []string) {
-			cpFunc(args[0], args[1], beginDate.ToDate(), endDate.ToDate())
-		},
 		Args: cobra.MinimumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			cpFunc(args[0], args[1], c.beginDate.ToDate(), c.endDate.ToDate())
+		},
 	}
-	mvCmd.Flags().VarPF(&beginDate, "begin-date", "b", "only move/merge dates after or equal to this")
-	mvCmd.Flags().VarPF(&endDate,   "end-date",   "e", "only move/merge dates before or equal to this")
-	app.AddCommand(mvCmd)
 
-
-	//------ CP COMMAND
 	cpCmd := &cobra.Command{
-		Use:   "cp <source-group>... <destination-group>",
-		Short: "copy/merge groups",
-		Run: func(cmd *cobra.Command, args []string) {
-			cpFunc(args[0], args[1], beginDate.ToDate(), endDate.ToDate())
-		},
 		Args: cobra.MinimumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			cpFunc(args[0], args[1], c.beginDate.ToDate(), c.endDate.ToDate())
+		},
 	}
-	cpCmd.Flags().VarPF(&beginDate, "begin-date", "b", "only copy dates after or equal to this")
-	cpCmd.Flags().VarPF(&endDate,   "end-date",   "e", "only copy dates before or equal to this")
-	app.AddCommand(cpCmd)
 
-	//------ RM COMMAND
 	rmCmd := &cobra.Command{
-		Use:   "rm <group>...",
-		Short: "remove a group",
+		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			delFunc(args[0])
 		},
-		Args: cobra.MinimumNArgs(1),
 	}
-	rmCmd.Flags()   .VarPF(&beginDate, "begin-date", "b", "only delete records after or equal to this")
-	rmCmd.Flags()   .VarPF(&endDate,   "end-date",   "e", "only delete records before or equal to this")
-	rmCmd.Flags().BoolVarP(&recursive, "recursive",  "r", false, "delete records in subgroups too")
 
-	app.AddCommand(rmCmd)
-
-	//------ AGG COMMAND
 	aggCmd := &cobra.Command{
-		Use:   "agg <group>...",
-		Short: "aggregate dates for range into single duration",
+		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			aggFunc(args[0], beginDate.String(), endDate.String())
+			aggFunc(args[0], c.beginDate.String(), c.endDate.String())
 			// viewFunc(args[0], beginDate.ToDate(), endDate.ToDate())
 		},
-		Args: cobra.ExactArgs(1),
 	}
 
-	aggCmd.Flags().VarPF   (&beginDate, "begin-date", "b", "only aggregate dates after or equal to this")
-	aggCmd.Flags().VarPF   (&endDate,   "end-date",   "e", "only aggregate dates before or equal to this")
-	aggCmd.Flags().BoolVarP(&recursive, "recursive",  "r", false, "aggregate includes all sub groups recursively")
-	aggCmd.Flags().BoolVarP(&daily,     "daily",      "d", false, "aggregate per day instead of all together")
+    // Set all the flags.
+	aggCmd .Flags().BoolVarP(&c.daily,     "daily",      "d", false, "aggregate per day instead of all together")
+	aggCmd .Flags().BoolVarP(&c.recursive, "recursive",  "r", false, "aggregate includes all sub groups recursively")
+	aggCmd .Flags().VarPF   (&c.beginDate, "begin-date", "b",        "only aggregate dates after or equal to this")
+	aggCmd .Flags().VarPF   (&c.endDate,   "end-date",   "e",        "only aggregate dates before or equal to this")
+	cpCmd  .Flags().VarPF   (&c.beginDate, "begin-date", "b",        "only copy dates after or equal to this")
+	cpCmd  .Flags().VarPF   (&c.endDate,   "end-date",   "e",        "only copy dates before or equal to this")
+	listCmd.Flags().BoolVarP(&c.quote,     "quote",      "q", false, "quote each group according to posix shell quoting rules")
+	mvCmd  .Flags().VarPF   (&c.beginDate, "begin-date", "b",        "only move/merge dates after or equal to this")
+	mvCmd  .Flags().VarPF   (&c.endDate,   "end-date",   "e",        "only move/merge dates before or equal to this")
+	rmCmd  .Flags().BoolVarP(&c.recursive, "recursive",  "r", false, "delete records in subgroups too")
+	rmCmd  .Flags().VarPF   (&c.beginDate, "begin-date", "b",        "only delete records after or equal to this")
+	rmCmd  .Flags().VarPF   (&c.endDate,   "end-date",   "e",        "only delete records before or equal to this")
+
+	addCmd.Use     = "add <group> <date> <duration>"
+	aggCmd.Use     = "agg <group>..."
+	app.Use        = "ttrack [command]"
+	cpCmd.Use      = "cp <source-group>... <destination-group>"
+	listCmd.Use    = "ls [<group>]..."
+	mvCmd.Use      = "mv <source-group>... <destination-group>"
+	recCmd.Use     = "rec <group> <duration>"
+	rmCmd.Use      = "rm <group>..."
+	setCmd.Use     = "set <group> <date> <duration>"
+	subCmd.Use     = "sub <group> <date> <duration>"
+	tidyCmd.Use    = "tidy"
+	versionCmd.Use = "version"
+
+	addCmd.Short     = "adds the duration for a group's date"
+	aggCmd.Short     = "aggregate dates for range into single duration"
+	app.Short        = "A time tracking program."
+	cpCmd.Short      = "copy/merge groups"
+	listCmd.Short    = "list groups"
+	mvCmd.Short      = "rename/merge groups"
+	recCmd.Short     = "record current time"
+	rmCmd.Short      = "remove a group"
+	setCmd.Short     = "sets the duration for a group's date"
+	subCmd.Short     = "subtracts the duration for a group's date"
+	tidyCmd.Short    = "clean up all the files ttrack uses"
+	versionCmd.Short = "print build information"
+
+	app.AddCommand(addCmd)
 	app.AddCommand(aggCmd)
+	app.AddCommand(cpCmd)
+	app.AddCommand(listCmd)
+	app.AddCommand(mvCmd)
+	app.AddCommand(recCmd)
+	app.AddCommand(rmCmd)
+	app.AddCommand(setCmd)
+	app.AddCommand(subCmd)
+	app.AddCommand(tidyCmd)
+	app.AddCommand(versionCmd)
 
 	app.Execute()
 }
