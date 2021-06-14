be3195003d8158efd4f12e454eb2e142872a6b1c
Alan Morgan <alanxoc3@gmail.com>
2021-06-13 21:22:53 -0600

    [GH-8] Second Tests.

--- internal/seconds/seconds.go
+++ internal/seconds/seconds.go
@@ -14,17 +14,29 @@ func CreateFromDuration(d time.Duration) Seconds {
 	return Seconds(d.Milliseconds() / 1000)
 }
 
+// Used with files.
+func CreateFromString(s string) Seconds {
+    dur, _ := time.ParseDuration(s) // TODO: Should something be done with an error?
+    return CreateFromDuration(dur)
+}
+
+// Used with the cache.
 func CreateFromBytes(b []byte) Seconds {
 	if b == nil {
 		return 0
 	}
-	secs := Seconds(binary.BigEndian.Uint32(b))
-	if secs > SECONDS_IN_DAY {
-		secs = SECONDS_IN_DAY
-	}
-	return secs
+	return Seconds(binary.BigEndian.Uint32(b))
 }
 
+// Used with files and cache.
+func (s *Seconds) CapAtOneDay() Seconds {
+	if *s > SECONDS_IN_DAY {
+		return SECONDS_IN_DAY
+	}
+	return *s
+}
+
+// Used for writing to files.
 func (s *Seconds) String() string {
 	dur := time.Duration(*s) * time.Second
 	return dur.String()
