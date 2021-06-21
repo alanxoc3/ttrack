60549894f27b74d743bf227ef75ef4445ceb2b31
Alan Morgan <alanxoc3@gmail.com>
2021-06-20 20:06:29 -0600

    [GH-8] More work on files.

--- internal/ttfile/file.go
+++ internal/ttfile/file.go
@@ -17,9 +17,23 @@ func (dl dateList) Len() int { return len(dl) }
 func (dl dateList) Less(i, j int) bool { return dl[i].IsLessThan(dl[j]) }
 func (dl dateList) Swap(i, j int) { dl[i], dl[j] = dl[j], dl[i] }
 
-func AddTimeout(filename string, insertion_date date.Date, timeout seconds.Seconds) {
-	lines := map[date.Date]seconds.Seconds{}
-	date_list := dateList{}
+type dateSeconds struct {
+    Date date.Date
+    Seconds seconds.Seconds
+}
+
+func insertOrAdd(dateToSeconds map[date.Date]seconds.Seconds, day date.Date, secs seconds.Seconds) {
+	if day.IsZero() || secs == 0 { return }
+
+	if val, ok := dateToSeconds[day]; ok {
+		dateToSeconds[day] = (val + secs).CapAtOneDay()
+	} else {
+		dateToSeconds[day] = secs.CapAtOneDay()
+	}
+}
+
+func GetDateSeconds(filename string) map[date.Date]seconds.Seconds {
+	dateToSeconds := map[date.Date]seconds.Seconds{}
 
 	if f, err := os.Open(filename); err == nil {
 		scanner := bufio.NewScanner(f)
@@ -32,14 +46,7 @@ func AddTimeout(filename string, insertion_date date.Date, timeout seconds.Secon
 				// TODO: Log errors?
 				d, _ := date.CreateFromString(strings.TrimSpace(tokens[0]))
 				s := seconds.CreateFromString(strings.TrimSpace(tokens[1]))
-				if d != nil && !d.IsZero() && s != 0 {
-					if val, ok := lines[*d]; ok {
-						lines[*d] = val + s
-					} else {
-						lines[*d] = s
-						date_list = append(date_list, *d)
-					}
-				}
+				if d != nil { insertOrAdd(dateToSeconds, *d, s) }
 			} else {
 				// TODO: Log errors?
 			}
@@ -53,14 +60,18 @@ func AddTimeout(filename string, insertion_date date.Date, timeout seconds.Secon
 		panic(err)
 	}
 
-	if !insertion_date.IsZero() && timeout != 0 {
-		if val, ok := lines[insertion_date]; ok {
-			lines[insertion_date] = val + timeout
-		} else {
-			lines[insertion_date] = timeout
-			date_list = append(date_list, insertion_date)
-		}
-	}
+    return dateToSeconds
+}
+
+func AddTimeout(filename string, insertion_date date.Date, timeout seconds.Seconds) {
+	date_list := dateList{}
+    lines := GetDateSeconds(filename)
+
+    insertOrAdd(lines, insertion_date, timeout)
+
+    for k := range lines {
+        date_list = append(date_list, k)
+    }
 
 	sort.Sort(date_list)
 
