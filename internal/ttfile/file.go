24c75fe59d84660067c635d51301a8aad2f05cac
Alan Morgan <alanxoc3@gmail.com>
2021-07-11 09:03:42 -0600

    [GH-8] Rec command puts data files into the correct directory now.

--- internal/ttfile/file.go
+++ internal/ttfile/file.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"sort"
 	"strings"
 
@@ -12,17 +13,20 @@ import (
 )
 
 type dateList []types.Date
-func (dl dateList) Len() int { return len(dl) }
+
+func (dl dateList) Len() int           { return len(dl) }
 func (dl dateList) Less(i, j int) bool { return dl[i].IsLessThan(dl[j]) }
-func (dl dateList) Swap(i, j int) { dl[i], dl[j] = dl[j], dl[i] }
+func (dl dateList) Swap(i, j int)      { dl[i], dl[j] = dl[j], dl[i] }
 
 type dateSeconds struct {
-    Date types.Date
-    DaySeconds types.DaySeconds
+	Date       types.Date
+	DaySeconds types.DaySeconds
 }
 
 func insertOrAdd(dateToSeconds map[types.Date]types.DaySeconds, day types.Date, secs types.DaySeconds) {
-	if day.IsZero() || secs.IsZero() { return }
+	if day.IsZero() || secs.IsZero() {
+		return
+	}
 
 	if val, ok := dateToSeconds[day]; ok {
 		dateToSeconds[day] = val.Add(secs)
@@ -45,7 +49,9 @@ func GetDateSeconds(filename string) map[types.Date]types.DaySeconds {
 				// TODO: Log errors?
 				d, _ := types.CreateDateFromString(strings.TrimSpace(tokens[0]))
 				s := types.CreateSecondsFromString(strings.TrimSpace(tokens[1]))
-				if d != nil { insertOrAdd(dateToSeconds, *d, s) }
+				if d != nil {
+					insertOrAdd(dateToSeconds, *d, s)
+				}
 			} else {
 				// TODO: Log errors?
 			}
@@ -59,31 +65,36 @@ func GetDateSeconds(filename string) map[types.Date]types.DaySeconds {
 		panic(err)
 	}
 
-    return dateToSeconds
+	return dateToSeconds
 }
 
 func AddTimeout(filename string, insertion_date types.Date, timeout types.DaySeconds) {
 	date_list := dateList{}
-    lines := GetDateSeconds(filename)
+	lines := GetDateSeconds(filename)
 
-    insertOrAdd(lines, insertion_date, timeout)
+	insertOrAdd(lines, insertion_date, timeout)
 
-    for k := range lines {
-        date_list = append(date_list, k)
-    }
+	for k := range lines {
+		date_list = append(date_list, k)
+	}
 
 	sort.Sort(date_list)
 
 	output_str := ""
 	for _, k := range date_list {
-    	v := lines[k]
+		v := lines[k]
 		output_str += k.String() + ": " + v.String() + "\n"
 	}
 
-	// Write to a file
-	err := os.WriteFile(filename, []byte(output_str), 0644)
-	if err != nil {
-		fmt.Println(err)
+    // Create dir with execute for cd, if it doesn't already exist.
+	if err := os.MkdirAll(filepath.Dir(filename), 0755); err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		return
+	}
+
+    // Create file with rw for user and r for everyone else (before umask).
+	if err := os.WriteFile(filename, []byte(output_str), 0644); err != nil {
+		fmt.Printf("ERROR: %s\n", err)
 		return
 	}
 }
