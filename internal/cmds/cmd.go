e50354858a04a6950a3164bef34dddf7e6ddf00e
Alan Morgan <alanxoc3@gmail.com>
2021-09-01 01:25:59 -0600

    [GH-8] Simplified rec logic to prepare for tidy command.

--- internal/cmds/cmd.go
+++ internal/cmds/cmd.go
@@ -1,10 +1,7 @@
 package cmds
 
 import (
-	"errors"
 	"fmt"
-	"io/fs"
-	"os"
 	"path/filepath"
 	"sort"
 	"time"
@@ -17,9 +14,10 @@ import (
 )
 
 type walkstrategy uint8
+
 const (
-    walk_level walkstrategy = iota
-    walk_recursive
+	walk_level walkstrategy = iota
+	walk_recursive
 )
 
 type State struct {
@@ -66,6 +64,12 @@ func CpFunc(s *State) {
 	*/
 }
 
+func TidyFunc(s *State) {
+	// What does tidy do?
+	// Anything in the cache that has exceeded its value goes to a file and that cache entry is deleted.
+	// All files are checked, sorted, and merging duplicates.
+}
+
 func SetFunc(s *State) {
 	ttfile.ModifyTime(filepath.Join(s.DataDir, s.Groups[0].Filename()), s.Date, func(ds types.DaySeconds) types.DaySeconds {
 		return s.Duration
@@ -88,19 +92,21 @@ func SubFunc(s *State) {
 func AggFunc(s *State) {
 	date_map := map[types.Date]types.DaySeconds{}
 
-    // STEP 1: Get the groups recursively.
+	// STEP 1: Get the groups recursively.
 	groupMap := walkThroughGroups(s.CacheDir, s.DataDir, s.Groups, walk_recursive)
 
-    // STEP 2: Populate from the cache.
+	// STEP 2: Populate from the cache.
 	ttdb.ViewCmd(s.CacheDir, func(tx *bolt.Tx) error {
-        for group := range groupMap {
-            b := tx.Bucket([]byte(group.String()))
-        	if b == nil { continue }
+		for group := range groupMap {
+			b := tx.Bucket([]byte(group.String()))
+			if b == nil {
+				continue
+			}
 
-            beg_ts, end_ts, timeout := expandGroup(b)
-    		_, _, duration, _ := recLogic(s.Now, beg_ts, end_ts, timeout)
+			beg_ts, end_ts, timeout := expandGroup(b)
+			duration, _ := calcDuration(s.Now, beg_ts, end_ts, timeout)
 
-    		date_key := *types.CreateDateFromTime(beg_ts)
+			date_key := *types.CreateDateFromTime(beg_ts)
 
 			if types.IsDateBetween(s.BeginDate, date_key, s.EndDate) {
 				if date_map_val, exists := date_map[date_key]; exists {
@@ -109,12 +115,12 @@ func AggFunc(s *State) {
 					date_map[date_key] = duration
 				}
 			}
-        }
+		}
 
-        return nil
+		return nil
 	})
 
-    // STEP 3: Populate from files.
+	// STEP 3: Populate from files.
 	for group := range groupMap {
 		local_date_map := ttfile.GetDateSeconds(filepath.Join(s.DataDir, group.Filename()))
 
@@ -129,25 +135,25 @@ func AggFunc(s *State) {
 		}
 	}
 
-    if s.Daily {
-    	dates := make(types.DateList, 0, len(date_map))
-    	for k := range date_map {
-    		dates = append(dates, k)
-    	}
+	if s.Daily {
+		dates := make(types.DateList, 0, len(date_map))
+		for k := range date_map {
+			dates = append(dates, k)
+		}
 
-    	sort.Sort(dates)
-    	for _, d := range dates {
-    		if v, ok := date_map[d]; ok && !v.IsZero() {
-    			fmt.Printf("%s: %s\n", d.String(), v.String())
-    		}
-    	}
-    } else {
-        totalSeconds := types.MultiDaySeconds{}
-    	for _, v := range date_map {
-            totalSeconds = totalSeconds.AddDaySeconds(v)
-    	}
-    	fmt.Printf("%s\n", totalSeconds.String())
-    }
+		sort.Sort(dates)
+		for _, d := range dates {
+			if v, ok := date_map[d]; ok && !v.IsZero() {
+				fmt.Printf("%s: %s\n", d.String(), v.String())
+			}
+		}
+	} else {
+		totalSeconds := types.MultiDaySeconds{}
+		for _, v := range date_map {
+			totalSeconds = totalSeconds.AddDaySeconds(v)
+		}
+		fmt.Printf("%s\n", totalSeconds.String())
+	}
 }
 
 func DelFunc(s *State) {
@@ -163,89 +169,6 @@ func DelFunc(s *State) {
 	})
 }
 
-func getRelWithPanic(basepath, relpath string) string {
-	path, err := filepath.Rel(basepath, relpath)
-	if err != nil {
-		panic(err) // is this possible if data_dir is invalid?
-	}
-	return path
-}
-
-func getListOfGroups(data_dir string, strat walkstrategy) []string {
-	groups := []string{}
-
-	filepath.Walk(data_dir, func(path string, info os.FileInfo, e error) error {
-		if info == nil {
-			return filepath.SkipDir
-		}
-
-		group_name := getRelWithPanic(data_dir, path)
-		if types.IsValidGroupFile(group_name) || types.IsValidGroupFolder(group_name) {
-			group_cleaned_name := types.CreateGroupFromString(group_name).String()
-			groups = append(groups, group_cleaned_name)
-		}
-
-        if info.IsDir() && group_name != "." && (strat == walk_level || !types.IsValidGroupFolder(group_name)) {
-            return filepath.SkipDir
-        } else {
-    		return nil
-        }
-	})
-
-	return groups
-}
-
-func walkThroughGroups(cache_dir, data_dir string, groupdirs []types.Group, strat walkstrategy) map[types.Group]bool {
-	visited_groups := map[types.Group]bool{}
-
-	ttdb.ViewCmd(cache_dir, func(tx *bolt.Tx) error {
-		c := tx.Cursor()
-		for k, _ := c.First(); k != nil; k, _ = c.Next() {
-            cursor_group := types.CreateGroupFromString(string(k))
-            for _, group := range groupdirs {
-                for _, ancestor := range cursor_group.GetAncestors(group) {
-                    visited_groups[ancestor] = true
-                    if strat == walk_level && ancestor != group { break }
-                }
-            }
-		}
-
-		return nil
-	})
-
-	for _, groupdir := range groupdirs {
-    	// Check for the group itself. It could be a folder or a .tt file.
-        if !groupdir.IsZero() {
-            _, folder_err := os.Stat(filepath.Join(data_dir, groupdir.Filename()));
-            _, file_err   := os.Stat(filepath.Join(data_dir, groupdir.String()));
-
-    		if !errors.Is(folder_err, fs.ErrNotExist) || !errors.Is(file_err, fs.ErrNotExist) {
-    			if _, exists := visited_groups[groupdir]; !exists {
-    				visited_groups[groupdir] = true
-    			}
-    		}
-        }
-
-        // Check the cache. Exact match or begins with .String() + "/".
-        // Do I need to split up the groups based on subdirs? Yes I do.
-        // How do I keep the order pretty?
-        // I probably have to load everything up at startup.
-
-        // Check for all sub groups.
-		groupdir_str := groupdir.String()
-		groups := getListOfGroups(filepath.Join(data_dir, groupdir_str), strat)
-		for _, group := range groups {
-			group_with_path := types.CreateGroupFromString(filepath.Join(groupdir_str, group))
-
-			if _, exists := visited_groups[group_with_path]; !exists {
-				visited_groups[group_with_path] = true
-			}
-		}
-	}
-
-	return visited_groups
-}
-
 func LsFunc(s *State) {
 	groups := s.Groups
 	if len(groups) == 0 {
@@ -254,7 +177,7 @@ func LsFunc(s *State) {
 
 	strat := walk_level
 	if s.Recursive {
-        strat = walk_recursive
+		strat = walk_recursive
 	}
 
 	group_map := walkThroughGroups(s.CacheDir, s.DataDir, groups, strat)
@@ -265,14 +188,13 @@ func LsFunc(s *State) {
 	sort.Strings(group_list)
 
 	for _, v := range group_list {
-        fmt.Println(v)
+		fmt.Println(v)
 	}
 
 }
 
+// Updates the cache or text file based on current time.
 func RecFunc(s *State) {
-	// Update cache.
-	// If new, append to txt file.
 	var timestamp_to_write *types.Date
 	var seconds_to_write *types.DaySeconds
 
@@ -280,27 +202,30 @@ func RecFunc(s *State) {
 	timeout_param := s.Duration
 
 	ttdb.UpdateCmd(s.CacheDir, func(tx *bolt.Tx) error {
-		// If the bucket doesn't exist and the timeout is zero, do nothing.
-		b, err := getOrCreateBucketConditionally(tx, group.String(), timeout_param.IsZero())
-		if b == nil || err != nil {
-			return err
-		}
+        // STEP 1: Create bucket if timeout is non-zero/valid.
+    	b := tx.Bucket([]byte(group.String()))
+    	if b == nil {
+            if timeout_param.IsZero() { return nil }
+    		var err error
+    		b, err = tx.CreateBucket([]byte(group.String()))
+    		if err != nil { return err }
+    	}
 
-		old_beg_ts, old_end_ts, old_timeout := expandGroup(b)
-		beg_ts, end_ts, duration, finish := recLogic(s.Now, old_beg_ts, old_end_ts, old_timeout)
+        // STEP 2: Calc duration.
+		beg_ts, end_ts, timeout := expandGroup(b)
+		duration, isFinal := calcDuration(s.Now, beg_ts, end_ts, timeout)
 
-		if finish && !duration.IsZero() {
-			timestamp_to_write = types.CreateDateFromTime(old_beg_ts)
+		if !duration.IsZero() {
+			timestamp_to_write = types.CreateDateFromTime(beg_ts)
 			seconds_to_write = &duration
 		}
 
-		if !old_beg_ts.Equal(beg_ts) {
-			ttdb.SetTimestamp(b, "beg", beg_ts)
-		}
-		ttdb.SetTimestamp(b, "end", end_ts)
+        // STEP 3: Set beg, end, and out.
+		ttdb.SetTimestamp(b, "end", s.Now)
+		ttdb.SetSeconds(b, "out", timeout_param)
 
-		if !timeout_param.IsZero() {
-			ttdb.SetSeconds(b, "out", timeout_param)
+		if isFinal || beg_ts.IsZero() || end_ts.IsZero() || timeout.IsZero() {
+			ttdb.SetTimestamp(b, "beg", s.Now)
 		}
 
 		return nil
