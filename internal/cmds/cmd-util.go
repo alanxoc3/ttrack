5f0665ab3cee7325db477ff0c2b332f823bf0dc9
Alan Morgan <alanxoc3@gmail.com>
2021-09-26 09:04:13 -0600

    [GH-8] Removed dead code.

--- internal/cmds/cmd-util.go
+++ internal/cmds/cmd-util.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"io/fs"
 	"os"
-	"strings"
 	"time"
 
 	"path/filepath"
@@ -23,47 +22,17 @@ type bucketInterface interface {
 // duration -> the duration that should be saved.
 // final -> is the duration final/should beg_ts restart?
 func calcDuration(now, beg_ts, end_ts time.Time, timeout types.DaySeconds) (types.DaySeconds, bool) {
-    if beg_ts.IsZero() || end_ts.IsZero() || timeout.IsZero() || now.Before(end_ts) || end_ts.Before(beg_ts) {
-        return types.DaySeconds{}, false
-    } else if now.Sub(end_ts).Seconds() > float64(timeout.GetAsUint32()) {
+	if beg_ts.IsZero() || end_ts.IsZero() || timeout.IsZero() || now.Before(end_ts) || end_ts.Before(beg_ts) {
+		return types.DaySeconds{}, false
+	} else if now.Sub(end_ts).Seconds() > float64(timeout.GetAsUint32()) {
 		return types.CreateSecondsFromUint32(uint32(end_ts.Sub(beg_ts).Seconds())).Add(timeout), true
 	} else {
-    	return types.CreateSecondsFromUint32(uint32(now.Sub(beg_ts).Seconds())), false
+		return types.CreateSecondsFromUint32(uint32(now.Sub(beg_ts).Seconds())), false
 	}
-}
-
-func addSecondToMap(m map[string]types.DaySeconds, key string, num types.DaySeconds) {
-	if num.IsZero() {
-		return
-	}
-	var base_val types.DaySeconds
-	if v, ok := m[key]; ok {
-		base_val = v
-	}
-	m[key] = base_val.Add(num)
-}
-
-func getGroupBucket(tx *bolt.Tx, group string) *bolt.Bucket {
-	return tx.Bucket([]byte(group))
 }
 
 func expandGroup(b *bolt.Bucket) (time.Time, time.Time, types.DaySeconds) {
 	return ttdb.GetTimestamp(b, "beg"), ttdb.GetTimestamp(b, "end"), ttdb.GetSeconds(b, "out")
-}
-
-func getGroupRecBucket(tx *bolt.Tx, group string) *bolt.Bucket {
-	gb := tx.Bucket([]byte(group))
-	if gb == nil {
-		return nil
-	}
-
-	rb := gb.Bucket([]byte("rec"))
-	return rb
-}
-
-func is_date_str_in_range(date, beg_date, end_date string) bool {
-	return (beg_date == "" || strings.Compare(beg_date, date) <= 0) &&
-		(end_date == "" || strings.Compare(end_date, date) >= 0)
 }
 
 func getRelWithPanic(basepath, relpath string) string {
@@ -104,85 +73,56 @@ func getListOfGroups(data_dir string, strat walkstrategy) []string {
 func walkThroughGroups(cache_dir, data_dir string, groupdirs []types.Group, strat walkstrategy, cached, stored bool) map[types.Group]bool {
 	visited_groups := map[types.Group]bool{}
 
-    if cached && (strat == walk_level || strat == walk_recursive) {
-    	ttdb.ViewCmd(cache_dir, func(tx *bolt.Tx) error {
-    		c := tx.Cursor()
-    		for k, _ := c.First(); k != nil; k, _ = c.Next() {
-    			cursor_group := types.CreateGroupFromString(string(k))
-    			for _, group := range groupdirs {
-    				for _, ancestor := range cursor_group.GetAncestors(group) {
-    					visited_groups[ancestor] = true
-    					if strat == walk_level && ancestor != group {
-    						break
-    					}
-    				}
-    			}
-    		}
+	if cached && (strat == walk_level || strat == walk_recursive) {
+		ttdb.ViewCmd(cache_dir, func(tx *bolt.Tx) error {
+			c := tx.Cursor()
+			for k, _ := c.First(); k != nil; k, _ = c.Next() {
+				cursor_group := types.CreateGroupFromString(string(k))
+				for _, group := range groupdirs {
+					for _, ancestor := range cursor_group.GetAncestors(group) {
+						visited_groups[ancestor] = true
+						if strat == walk_level && ancestor != group {
+							break
+						}
+					}
+				}
+			}
 
-    		return nil
-    	})
-    }
+			return nil
+		})
+	}
 
-    if stored {
-    	for _, groupdir := range groupdirs {
-    		// Check for the group itself. It could be a folder or a .tt file.
-    		if !groupdir.IsZero() {
-    			_, file_err := os.Stat(filepath.Join(data_dir, groupdir.String()))
-    			if !errors.Is(file_err, fs.ErrNotExist) {
-    				if _, exists := visited_groups[groupdir]; !exists {
-    					visited_groups[groupdir] = true
-    				}
-    			}
+	if stored {
+		for _, groupdir := range groupdirs {
+			// Check for the group itself. It could be a folder or a .tt file.
+			if !groupdir.IsZero() {
+				_, file_err := os.Stat(filepath.Join(data_dir, groupdir.String()))
+				if !errors.Is(file_err, fs.ErrNotExist) {
+					if _, exists := visited_groups[groupdir]; !exists {
+						visited_groups[groupdir] = true
+					}
+				}
 
-    			_, folder_err := os.Stat(filepath.Join(data_dir, groupdir.Filename()))
-    			if (strat == walk_level || strat == walk_recursive) && !errors.Is(folder_err, fs.ErrNotExist) {
-    				if _, exists := visited_groups[groupdir]; !exists {
-    					visited_groups[groupdir] = true
-    				}
-    			}
-    		}
+				_, folder_err := os.Stat(filepath.Join(data_dir, groupdir.Filename()))
+				if (strat == walk_level || strat == walk_recursive) && !errors.Is(folder_err, fs.ErrNotExist) {
+					if _, exists := visited_groups[groupdir]; !exists {
+						visited_groups[groupdir] = true
+					}
+				}
+			}
 
-    		// Check for all sub groups.
-    		groupdir_str := groupdir.String()
-    		groups := getListOfGroups(filepath.Join(data_dir, groupdir_str), strat)
-    		for _, group := range groups {
-    			group_with_path := types.CreateGroupFromString(filepath.Join(groupdir_str, group))
+			// Check for all sub groups.
+			groupdir_str := groupdir.String()
+			groups := getListOfGroups(filepath.Join(data_dir, groupdir_str), strat)
+			for _, group := range groups {
+				group_with_path := types.CreateGroupFromString(filepath.Join(groupdir_str, group))
 
-    			if _, exists := visited_groups[group_with_path]; !exists {
-    				visited_groups[group_with_path] = true
-    			}
-    		}
-    	}
-    }
+				if _, exists := visited_groups[group_with_path]; !exists {
+					visited_groups[group_with_path] = true
+				}
+			}
+		}
+	}
 
 	return visited_groups
 }
-
-/*
-func getDateMap(tx *bolt.Tx, group, beg_bounds, end_bounds string) map[string]types.DaySeconds {
-	m := map[string]types.DaySeconds{}
-
-	gb := getGroupBucket(tx, group)
-	if gb == nil { return m }
-
-	beg_ts, end_ts, timeout, := expandGroup(gb)
-	_, _, duration, _ := recLogic(time.Now(), beg_ts, end_ts, timeout)
-	beg_ts_str := formatTimestamp(beg_ts)
-	if is_date_str_in_range(beg_ts_str, beg_bounds, end_bounds) {
-		addSecondToMap(m, formatTimestamp(beg_ts), duration)
-	}
-
-	if rec == nil { return m }
-
-	c := rec.Cursor()
-
-	for k, v := c.First(); k != nil; k, v = c.Next() {
-    		dateStr := string(k)
-    		if is_date_str_in_range(dateStr, beg_bounds, end_bounds) {
-			addSecondToMap(m, string(k), types.CreateFromBytes(v))
-    		}
-	}
-
-	return m
-}
-*/
